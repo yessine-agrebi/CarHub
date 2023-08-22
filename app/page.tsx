71f0ec7fc460a525cd7f961a2417136d24aa86d3
yessine <yessine.agrebi.aiesec@gmail.com>
2023-08-22 15:56:43 +0200

    car card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
-export default function Home() {
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
+export default async function Home() {
+  const allCars = await fetchCars()
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -15,6 +18,19 @@ export default function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
+      {!isDataEmpty ? (
+        <section>
+          <div className="home__cars-wrapper">
+            {allCars?.map((car) => <CarCard car={car} />)}
+          </div>
+        </section>
+      ): (
+        <div className="home__error-container">
+          <h2 className="text-black text-xl font-bold">Oops, no result</h2>
+          <p>{allCars?.message}</p>
+        </div>
+      )}
+
       </div>
     </main>
   );
--- components/index.ts
+++ components/index.ts
@@ -5,6 +5,8 @@ import Footer from "./Footer";
 import CustomFilter from "./CustomFilter";
 import SearchBar from "./SearchBar";
 import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 export {
     Hero,
     CustomButton,
@@ -12,5 +14,7 @@ export {
     Footer,
     CustomFilter,
     SearchBar,
-    SearchManufacturer
+    SearchManufacturer,
+    CarCard,
+    CarDetails
 }
