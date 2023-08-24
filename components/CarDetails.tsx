cd6a19d0b1108592b2ed439cf01a248ff0329108
yessine <yessine.agrebi.aiesec@gmail.com>
2023-08-24 14:36:54 +0200

    car modal

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -38,7 +38,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 <Dialog.Panel
                   className="relative w-full max-w-lg 
                 max-h-[90vh] overflow-auto transform
-                 rounded-2xl bg-white text-left shadow-xsl
+                 rounded-2xl bg-white p-6 text-left shadow-xl
                   transition-all flex flex-col gap-5"
                 >
                   <button
@@ -95,6 +95,20 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                       </div>
                     </div>
                   </div>
+                  <div className="flex-1 flex flex-col gap-2">
+                      <h2 className="font-semibold text-xl capitalize">
+                        {car.make} {car.model}
+                      </h2>
+                      <div className="mt-3 flex flex-wrap gap-4">
+                        {Object.entries(car).map(([key, value]) => (
+                          <div className="flex justify-between gap-5 w-full text-right" key={key}>
+                            <h4 className="text-grey capitalize">{key.split('_').join(' ')}</h4>
+                            <p className="text-black-100 font-semibold">{value}</p>
+                          </div>
+                        ))}
+                      </div>
+                  </div>
+
                 </Dialog.Panel>
               </Transition.Child>
             </div>
